4e9dce175b0cab00b8a4679ade99befced679246
dspringfield <asd>
2020-09-19 00:57:42 +0300

    check arguments and results of calculations

--- src/equation.cpp
+++ src/equation.cpp
@@ -3,20 +3,31 @@
 
 #include "equation.h"
 
+static bool isValidCoefficientsAndPointers(double a, double b, double c,
+                                           const double *roots,
+                                           double epsilon) {
+
+  return std::isfinite(a) && std::isfinite(b) && std::isfinite(c) &&
+         std::isfinite(epsilon) && roots != nullptr;
+}
+
 char solveUpToQuadraticEquation(double a, double b, double c, double *roots,
                                 double epsilon) {
-  assert(roots != nullptr);
   assert(std::isfinite(a));
   assert(std::isfinite(b));
-  assert(std::isfinite(c));
   assert(std::isfinite(epsilon));
+  assert(roots != nullptr);
+
+  if (!isValidCoefficientsAndPointers(a, b, c, roots, epsilon)) {
+    return INVALID_ARGUMENTS;
+  }
 
   if (!isZero(a, epsilon)) {
-    return solveQuadraticEquation(a, b, c, roots, epsilon);
+    return solveQuadraticEquation(a, b, c, roots, epsilon, true);
   }
 
   if (!isZero(b, epsilon)) {
-    return solveLinearEquation(b, c, roots, epsilon);
+    return solveLinearEquation(b, c, roots, epsilon, true);
   }
 
   if (!isZero(c, epsilon)) {
@@ -27,22 +38,32 @@ char solveUpToQuadraticEquation(double a, double b, double c, double *roots,
 }
 
 char solveQuadraticEquation(double a, double b, double c, double *roots,
-                            double epsilon) {
-  assert(roots != nullptr);
+                            double epsilon, bool checked) {
   assert(std::isfinite(a));
   assert(std::isfinite(b));
-  assert(std::isfinite(c));
   assert(std::isfinite(epsilon));
+  assert(roots != nullptr);
+
+  if (!checked && isValidCoefficientsAndPointers(a, b, c, roots, epsilon)) {
+    return INVALID_ARGUMENTS;
+  }
 
   if (isZero(a, epsilon)) {
     return LEADING_COEFFICIENT_IS_ZERO;
   }
 
   double discriminant = calculateDiscriminant(a, b, c);
+  if (!std::isfinite(discriminant)) {
+    return FAILURE;
+  }
 
   if (isZero(discriminant, epsilon)) {
     roots[0] = calculateParabolaXVertex(a, b);
-    return 1;
+    if (std::isfinite(roots[0])) {
+      return 1;
+    }
+
+    return FAILURE;
   }
 
   if (discriminant < 0) {
@@ -52,21 +73,33 @@ char solveQuadraticEquation(double a, double b, double c, double *roots,
   double sqrtDiscriminant = sqrt(discriminant);
   roots[0] = (-b - sqrtDiscriminant) / (2 * a);
   roots[1] = (-b + sqrtDiscriminant) / (2 * a);
-  return 2;
+  if (std::isfinite(roots[0]) && std::isfinite(roots[1])) {
+    return 2;
+  }
+  return FAILURE;
 }
 
-char solveLinearEquation(double a, double b, double *roots, double epsilon) {
-  assert(roots != nullptr);
+char solveLinearEquation(double a, double b, double *roots, double epsilon,
+                         bool checked) {
   assert(std::isfinite(a));
   assert(std::isfinite(b));
   assert(std::isfinite(epsilon));
+  assert(roots != nullptr);
+
+  if (!checked && isValidCoefficientsAndPointers(0, a, b, roots, epsilon)) {
+    return INVALID_ARGUMENTS;
+  }
 
   if (isZero(a, epsilon)) {
     return LEADING_COEFFICIENT_IS_ZERO;
   }
 
   roots[0] = -b / a;
-  return 1;
+  if (std::isfinite(roots[0])) {
+    return 1;
+  }
+
+  return FAILURE;
 }
 
 double calculateDiscriminant(double a, double b, double c) {
